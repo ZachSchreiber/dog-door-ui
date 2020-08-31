1f739eb34c0ad0a4b769bb6673b10eb8762aea0d
zachary schreiber <zschreiber@ebsco.com>
2020-08-31 08:53:22 -0400

    footer section

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -14,7 +14,20 @@ const Home = () => {
         </div>
       </div>
       <AboutSection />
-      <div className="footer"></div>
+      <div className="footer-section">
+        <h2 className="footer-section__section-header">What we do</h2>
+        <p className="footer-section__section-content">
+          skim the surface to this point. The target area is only two meters
+          wide. It’s a small thermal exhaust port, right below the main port.
+          The shaft leads directly to the reactor system. A precise hit will
+          start a chain reaction which should destroy the station. Only a
+          precise hit will set up a chain reaction. The shaft is ray-shielded,
+          so you’ll have to use proton torpedoes. That’s impossible, even for a
+          computer. It’s not impossible. I used to bull’s-eye womp rats in my
+          T-sixteen back home. They’re not much bigger than two meters. Man your
+          ships! And may the Force be with you!
+        </p>
+      </div>
     </>
   );
 };
